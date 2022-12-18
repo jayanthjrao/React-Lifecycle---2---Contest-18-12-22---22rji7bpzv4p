aae154dd044066ce8f165ceb2967f4f9bfe0c48f
jayanthjrao <118825461+jayanthjrao@users.noreply.github.com>
2022-12-18 19:02:57 +0530

    Update react.js

--- react.js
+++ react.js
@@ -1,3 +1,9 @@
+class MyComponent extends
+    React.component{
 componentDidMount(){
     console.log("mounted")
+    }
+       render(){
+           return<div>my component</div>;
+       }
     }
